876b3a93b8f9ee39373a12a0d9830f0312629d32
yinzhixiaxue <775288786@qq.com>
2017-02-14 09:29:22 +0800

    叶子下落

---
+++ leafdown/js/index.js
@@ -0,0 +1,13 @@
+require(["./leaf"],function(Leaf){
+    var oContainer=document.getElementById("container");
+    var winWidth=document.documentElement.clientWidth;
+    for(var i=0;i<4;i++){
+        var iWidth=50+parseInt(Math.random()*51);
+        var leaf=new Leaf({
+            width : iWidth,
+            left : parseInt((winWidth-iWidth)*Math.random()),
+            container :oContainer
+        });
+        leaf.fall();
+    }
+});
